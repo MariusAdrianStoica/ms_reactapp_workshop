1557bccb126db7f820871a2b6ffb6362cd3406ab
MariusAdrianStoica <stoichitza@yahoo.com>
2023-03-13 20:05:37 +0100

    Project setup done

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+
+const greetingMessage =<div>React_App_Workshop_Loading</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(greetingMessage);
